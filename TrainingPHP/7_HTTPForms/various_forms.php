73334e314ee1a721fdabb147a83b0216a04daac6
Valerii <valeradots81@gmail.com>
2018-05-16 11:11:01 +0300

    1.7.2

--- TrainingPHP/7_HTTPForms/various_forms.php
+++ TrainingPHP/7_HTTPForms/various_forms.php
@@ -6,6 +6,10 @@ if (isset($_POST['submit'])) {
     echo '<pre>';
     print_r($_POST);
     echo '</pre>';
+
+    echo '<pre>';
+    print_r($_FILES);
+    echo '</pre>';
 }
 ?>
 
@@ -19,16 +23,42 @@ if (isset($_POST['submit'])) {
 
 <body>
 <div>
-    <form action="various_forms.php" method="post">
-        <p>Enter Firstname and Lastname</p>
+    <form action="various_forms.php" method="post" enctype="multipart/form-data">
+        <p>Enter FirstName and LastName</p>
         <input type="text" name="firstname"><br>
         <input type="text" name="lastname"><br>
 
         <hr>
+        <p>Enter Password</p>
+        <input type="password" name="password"><br>
+
+        <hr>
+        <p>Select a method of forwarding:</p>
+        <input type="checkbox" name="transport[]" value="onFoot">On foot<br>
+        <input type="checkbox" name="transport[]" value="byCar">By car<br>
+        <input type="checkbox" name="transport[]" value="metro">Metro<br>
+
+        <hr>
+        <p>Choose your gender:</p>
+        <input type="radio" name="gender" value="male">Male<br>
+        <input type="radio" name="gender" value="female">Female<br>
+
+        <hr>
+        <p>Select city:</p>
+        <select name="city">
+            <option value="london">London</option>
+            <option value="paris">Paris</option>
+            <option value="kyiv">Kyiv</option>
+        </select>
+
+        <hr>
+        <p>Attach the file:</p>
+        <input type="file" name="file">
 
 
-        <input type="reset" value="Reset">
-        <input type="submit" value="Submit" name="submit">
+        <hr>
+        <input type="reset" value="reset">
+        <input type="submit" value="submit" name="submit">
     </form>
 </div>
 </body>
